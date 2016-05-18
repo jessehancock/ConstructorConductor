d783dcb16429f5da339a9836b37ce6429a0dcc24
jessehancock <jessehancock1@gmail.com>
2016-05-18 17:59:30 -0600

    finsihed

--- quizApp.js
+++ quizApp.js
@@ -6,26 +6,41 @@
 //Create a QuizUser constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
   //code here
+var QuizUser = function(name, email, password, totalScore){
+	this.name = name;
+	this.email = email;
+	this.password = password;
+	this.totalScore = totalScore;
+}
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
-  //code here
+var Question = function(title, answersArray, rightAnswer, difficulty){
+	this.title = title;
+	this.answersArray = answersArray;
+	this.rightAnswer = rightAnswer;
+	this.difficulty = difficulty;
+}
 
 
 //Create a quizUsers Array which is going to hold all of our users.
 
-  //code here
+var quizUsers = [];
 
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
-  //code here
-
+var user1 = new QuizUser('jesse', 'jhancock@gmail', '123', 43);
+var user2 = new QuizUser('carly', 'cahancok@gmail', '12345', 41);
+var user3 = new QuizUser('baby', 'baby@gmial', '12', 0);
+quizUsers.push(user1);
+quizUsers.push(user2);
+quizUsers.push(user3);
 
 //Create a questions Array which is going to hold all of our questions
 
-  //code here
+var questions = [];
 
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
@@ -35,6 +50,12 @@
 //Fill in the rest of the required data as you see appropriate.
 
   //code here
+var Q1 = new Question('T/F: Inheritance is achieved in JavaScript through Prototypes?',['T','F'], 'T', 1);
+
+var Q2 = new Question('T/F: JavaScript is just a scripting version of Java', ['T','F'], 'T', 1);
+
+var Q3 = new Question("T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value", ['T','F'], 'T', 1);
+questions.push(Q1, Q2, Q3);
 
 
 //Now push all of your instances of Question into the questions Array
